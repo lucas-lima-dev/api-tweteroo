ddcd14dfda75106176a33c30c7393e812b270263
Lucas Lima <lucaslima@MacBook-Pro-de-Lucas.local>
2023-08-28 19:57:48 -0300

    create root file

---
+++ src/main/java/com/apitweteroo/apitweteroo/ApitweterooApplication.java
@@ -0,0 +1,14 @@
+package com.apitweteroo.apitweteroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitweterooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitweterooApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/apitweteroo/apitweteroo/ApitweterooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apitweteroo.apitweteroo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitweterooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
